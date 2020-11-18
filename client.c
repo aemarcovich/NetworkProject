23990a08026a87555d1a8d3f380e23cd8a88f2ae
Alejandro Marcovich <aemarcovich@dons.usfca.edu>
2020-11-17 20:07:58 -0800

    adding updates

--- client.c
+++ client.c
@@ -16,9 +16,21 @@
 
 #define PORT "3490" // the port client will be connecting to
 
- #define MAXDATASIZE 1024 // max number of bytes we can get at once
+ #define MAXDATASIZE 1000 // max number of bytes we can get at once
 
+int UDP_packets=20;//match with server
  // get sockaddr, IPv4 or IPv6:
+char serverip[MAXDATASIZE];
+int sport;
+int dport;
+int dport_tcp;
+int dport_tcp_t;
+char tcpport[MAXDATASIZE];
+int udpsize;
+int intertime;
+int TTL;
+char data[MAXDATASIZE]={0};
+double time_taken;
 void *get_in_addr(struct sockaddr *sa)
 {
 	if (sa->sa_family == AF_INET) {
@@ -27,25 +39,52 @@ void *get_in_addr(struct sockaddr *sa)
 
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
+void setvar(FILE *fp){
+  char local_data[MAXDATASIZE]={0};
+  char* token;
+  fread(data,1,MAXDATASIZE,fp);
+  strcpy(local_data,data);
+  char* rest =local_data;
+  int line=0;
+  while(token=strtok_r(rest,"\n",&rest)){
+    if(line==0)
+      strcpy(serverip,token);
+    else if(line==1)
+      sport=atoi(token);
+    else if(line==2)
+      dport=atoi(token);
+    else if(line==3)
+      dport_tcp=atoi(token);
+     else if(line==4)
+      dport_tcp_t=atoi(token);
+     else if(line==5)
+      strcpy(tcpport,token);
+     else if(line==6)
+      udpsize=atoi(token);
+     else if(line==7)
+      intertime=atoi(token);
+     else if(line==8)
+      UDP_packets=atoi(token);
+     else if(line==9)
+      TTL=atoi(token);
+    line++;
+  }
+}
 void send_file(FILE *fp,int sockfd){
-	char data[MAXDATASIZE] = {0};
-
-	while(fgets(data,MAXDATASIZE,fp) != NULL){
-		if(send(sockfd,data, sizeof(data),0) == -1){
+		if(send(sockfd,data, strlen(data),0) == -1){
 			perror("-Error with sending data");
 			exit(1);
 		}
-		printf("file sent %s\n", data);
+		printf("file sent\n%s\n", data);
 		bzero(data, MAXDATASIZE);
-	}
-	//sleep(10);
 }
+
 void send_udp(){
 
   int port = 8765;
   int sockfd;
   struct sockaddr_in serverAddr,srcaddr;
-  char buffer[1024];
+  char buffer[1000];
   socklen_t addr_size;
 
   sockfd = socket(PF_INET, SOCK_DGRAM, 0);
@@ -53,8 +92,8 @@ void send_udp(){
   //set destport
   memset(&serverAddr, '\0', sizeof(serverAddr));
   serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(port);
-  serverAddr.sin_addr.s_addr = inet_addr("192.168.137.129");
+  serverAddr.sin_port = htons(dport);
+  serverAddr.sin_addr.s_addr = inet_addr(serverip);
   
   //set Don't flag
   int val = IP_PMTUDISC_DO;
@@ -64,53 +103,76 @@ void send_udp(){
    memset(&srcaddr, 0, sizeof(srcaddr));
    srcaddr.sin_family = AF_INET;
    srcaddr.sin_addr.s_addr = inet_addr("192.168.137.128");
-   srcaddr.sin_port = htons(9876);
+   srcaddr.sin_port = htons(sport);
 
-    // if (bind(sockfd, (struct sockaddr *) &srcaddr, sizeof(srcaddr)) < 0) {
-    //     perror("bind");
-    //     exit(1);
-    // }
-    // for(int t=0; t<6000; t++)
-    // {
-    // 	//buffe
-    // 	int zeropacket[1000]={0};
-    // 	uint16_t *packet_id = zeropacket;
-    // 	*packet_id = t;
-    // 	memset((zero_train+1000)*t);
-    // 	sendto(sockfd, zeropacket, 1024, 0,(struct sockaddr*)&serverAddr,sizeof(serverAddr));
-    // }
-    // sleep(15);
-    // for(int t=0; t<6000; t++)
-    // {
-    // 	//buffe
-    // 	char randtrain[1000];
-    // 	uint16_t *packet_id = randtrain;
-    // 	*packet_id = t;
-    // 	for(int i=2;i<sizeof(randtrain);i++)
-    // 	{
-    // 	//genterate rand int random bit
-    // 		int num = (rand() % (256));
-    // 		randtrain[i]=num;
-    // 	}
-    // 	sendto(sockfd, randtrain 1024, 0,(struct sockaddr*)&serverAddr,sizeof(serverAddr));
-    // }
-    // printf("data print\n");
+    if (bind(sockfd, (struct sockaddr *) &srcaddr, sizeof(srcaddr)) < 0) {
+        perror("bind");
+        exit(1);
+    }
+    char zero_train[UDP_packets][udpsize];
 
-  strcpy(buffer,"testing UDP to server\n");
-  sendto(sockfd, buffer, 1024, 0,(struct sockaddr*)&serverAddr,sizeof(serverAddr));
-  printf("[+]Data Send: %s", buffer);
-  addr_size = sizeof(serverAddr);
-  recvfrom(sockfd, buffer, 1024, 0,(struct sockaddr*)&serverAddr, &addr_size);
-  printf("[+]Data Received: %s\n", buffer);
+    for(int t=0; t<UDP_packets; t++)
+    {
+    	//buffe
+    	char zeropacket[udpsize];
+      memset(zeropacket,0,udpsize);
+    	uint16_t *packet_id = (uint16_t*) zeropacket;
+    	*packet_id = t;
+    	memcpy(zero_train[t], zeropacket, udpsize);
+    }
+    for (int i = 0; i < UDP_packets; ++i)
+    {
+      sendto(sockfd, zero_train[i], udpsize, 0,(struct sockaddr*)&serverAddr,sizeof(serverAddr));
+    }
+    sleep(intertime);
+
+    //char randtrain[UDP_packets][udpsize];
+
+    for(int t=0; t<UDP_packets; t++)
+    {
+     //buffe
+      
+      char randpacket[udpsize];
+     uint16_t *packet_id = (uint16_t*) randpacket;
+     *packet_id = t;
+     for(int i=2;i<udpsize;i++)
+     {
+     //genterate rand int random bit
+       char num = (rand() % (256));
+       randpacket[i]=num;
+     }
+      memcpy(zero_train[t], randpacket, udpsize);
+    }
+    for (int i = 0; i < UDP_packets; ++i)
+    {
+      sendto(sockfd, zero_train[i], udpsize, 0,(struct sockaddr*)&serverAddr,sizeof(serverAddr));
+    }
+
 }
-// void sendPackets(char *data,int sockfd){
-// 	if(send(sockfd,data, sizeof(data),0) == -1){
-// 		perror("-Error with sendig data");
-// 		exit(1);
-// 	}
-// 	bzero(data, MAXDATASIZE);
-// 	printf("file sent\n");
-// }
+
+void recv_value(int sockfd){
+  char message[500];
+  char timer[1000];
+  strcpy(message,"send the time\n");
+    if(send(sockfd,message, strlen(message),0) == -1){
+      perror("-Error with sending data");
+      exit(1);
+    }
+    printf("message  sent\n");
+    recv(sockfd,timer,strlen(message),0);
+    time_taken=atof(timer);
+    printf("%lf seconds\n",time_taken );
+    double check=.1;
+    if(time_taken>check)
+    {
+      printf("Compression detected");
+    }
+    else{
+      printf("Compression not detected\n");
+    }
+    bzero(message, 500);
+}
+
 int main(int argc, char *argv[])
 {
 	int sockfd, numbytes;
@@ -119,7 +181,7 @@ int main(int argc, char *argv[])
 	int rv;
 	char s[INET6_ADDRSTRLEN];
 	FILE *fp;
-	char *filename="test.txt";
+	char *filename=argv[1];
 
 	char buffer[1024];
 	struct sockaddr_in serverAddr;
@@ -129,12 +191,20 @@ int main(int argc, char *argv[])
 		fprintf(stderr,"usage: client hostname\n");
 		exit(1);
 	}
+
+ fp= fopen(filename,"r");
+
+ if(fp==NULL){
+  perror("error with reading");
+  exit(1);
+ }
+  setvar(fp);
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 
 
-	if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+	if ((rv = getaddrinfo(serverip, tcpport, &hints, &servinfo)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 		return 1;
 	}
@@ -171,30 +241,15 @@ printf("client: connecting to %s\n", s);
  	exit(1);
  }
 
- fp= fopen(filename,"r");
-
- if(fp==NULL){
- 	perror("error with reading");
- 	exit(1);
- }
- // send_file(fp,sockfd);
- // sleep(10);
-
+ send_file(fp,sockfd);
+ fclose(fp);
+ sleep(10);
+ printf("sending UDP_packets: please wait\n");
  send_udp();
-//  sockfd = socket(p->ai_family, SOCK_DGRAM,p->ai_protocol);
-
-// strcpy(buffer,"testing UDP to server\n");
-// sendto(sockfd, buffer, 1024, 0,(struct sockaddr*)&serverAddr,sizeof(serverAddr));
-// printf("Data has been sent: %s\n", buffer);
+ recv_value(sockfd);
 
 
  buf[numbytes] = '\0';
- // char data[6000*1000] = {0};
- // for(int x=1,x<=6000;x++)
- // {
- // 	createPackets(data);
- // 	sendPackets(data);
- // }
 
  printf("client: received '%s'\n",buf);
 
